8cc11ba635f89aee8a9c296967aedb38105ea155
c08oprkiua <85906487+c08oprkiua@users.noreply.github.com>
2023-12-29 19:10:55 -0700

    Idek

--- src/profiles/profile.hpp
+++ src/profiles/profile.hpp
@@ -1,3 +1,5 @@
+#include <cstdint>
+
 
 typedef struct URL_Patch
 {
@@ -5,64 +7,84 @@ typedef struct URL_Patch
     char url[80];
 } URL_Patch;
 
+struct ProfileMeta {
+    //bitfield; used URLs are 1, otherwise 0
+    //Idea being it is faster to check this to know which to patch when switching profiles
+    // than to attempt patching all the URLs, which a profile may or may not need to edit
+    uint32_t usedfields;
+
+    char name[15]; //Idk 15 seemed like a good amount
+};
+
 
 class Profile
 {
 public:
-    //bitfield; used URLs are 1, otherwise 0
-    //Idea being it is faster to check this to know which to patch when switching profiles
-    // than to attempt patching all the URLs, which a profile may or may not need to edit
-    unsigned int usedfields; 
 
-    char name[15]; //Idk 15 seemed like a good amount
+    ProfileMeta metadata;
 
     struct eshop {
-        char pushmore[80];
+        char pushmore[80]; // 1 << 0
         //Tagaya is the eshop applet
-        char tagaya[80];
-        char tagaya_latest[80];
+        char tagaya[80] ; // 1 << 1
+        char tagaya_latest[80]; // 1 << 2
         //Pushmo is related to title installs
-        char pushmo_d[80];
-        char pushmo_c[80];
+        char pushmo_d[80]; // 1 << 3
+        char pushmo_c[80]; // 1 << 4
         //
-        char ecs_wup[80]; //Exists twice in RAM, does not need to be repeated as a char value
-        char nus_wup[80];
+        char ecs_wup[80]; //Exists twice in RAM, does not need to be repeated as a char value. 1 << 5
+        char nus_wup[80]; // 1 << 6
         //c
-        char ecsc1[80];
-        char ecsc2[80];
-        char ias[80];
-        char cas[80];
-        char nus_c[80];
+        char ecsc1[80]; // 1 << 7
+        char ecsc2[80]; // 1 << 8 
+        char ias[80]; // 1 << 9
+        char cas[80]; // 1 << 10
+        char nus_c[80]; // 1 << 11
     };
-
     struct npns {
-        char npns1[80];
-        char npns2[80];
+        char npns1[80]; // 1 << 12
+        char npns2[80]; // 1 << 13
     };
 
     struct spotpass {
-        char nnpl[80];
-        char npts1[80];
-        char npts2[80];
-
+        char nnpl[80]; // 1 << 14
+        char npts1[80]; // 1 << 15
+        char npts2[80]; // 1 << 16
     };
 
     struct icondatabase {
-        char wup1[80];
-        char wup2[80];
-        char ctr1[80];
-        char ctr2[80];
+        char wup1[80]; // 1 << 17
+        char wup2[80]; // 1 << 18
+        char ctr1[80]; // 1 << 19
+        char ctr2[80]; // 1 << 20
     };
 
-
+    //All these values are the Miiverse defaults
     struct miiverse {
         //Discovery +v1/endpoint URL
-        char discurl[39] = "disc.olv.pretendo.cc/v1/endpoint";
-        //These are the color patches
-        char wave[29];
-        unsigned char highlight[16];
-        unsigned char touch1[8];
-        unsigned char touch2[8];
+        char discurl[39] = "disc.olv.nintendo.net/v1/endpoint"; // 1 << 21
+        //This is the base domain, eg. https.nintendo.net
+        char wave[29] = {
+        0x68, 0x74, 0x74, 0x70, 0x73, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x2E, 0x6E, 0x69, 0x6E, 0x74, 0x65, 0x6E, 0x64,
+        0x6F, 0x2E, 0x6E, 0x65, 0x74
+        }; // 1 << 22
+
+        //These are the color patches, stored in RGBA
+        unsigned char highlight[16] = {
+            0x82, 0xff, 0x05, 0xff, 
+            0x82, 0xff, 0x05, 0xff, 
+            0x1d, 0xff, 0x04, 0xff, 
+            0x1d, 0xff, 0x04, 0xff
+        }; // 1 << 23
+        unsigned char touch1[8] = {
+            0x94, 0xd9, 0x2a, 0x00, 
+            0x57, 0xbd, 0x12, 0xff
+        }; // 1 << 24
+        unsigned char touch2[8] = {
+            0x57, 0xbd, 0x12, 0x00, 
+            0x94, 0xd9, 0x2a, 0xff
+        }; // 1 << 25
     };
     
     void ValidateUsedFields(); //manually validates usedfields
--- src/profiles/profileloader.hpp
+++ src/profiles/profileloader.hpp
@@ -1,9 +1,9 @@
 #include "profile.hpp"
+#include <vector>
 
-//When profiles are loaded, they're simply stored as their order when read from storage.
-//This keeps them as a tiny reference value in RAM until we actually want them, at which point
-//we use the value in the array to fetch the actual values. This could cause some issues if the 
-//profiles get edited during runtime, but I don't think most people will do that... hopefully.
-unsigned int ProfileArray[];
+//When profiles are loaded, their metadata struct is loaded into this array. 
+//When a profile is actually requested, *then* the full Profile will be processed
+std::vector<ProfileMeta> ProfileArray;
 
+bool InitializeProfileArray();
 bool LoadProfileFromStorage();
